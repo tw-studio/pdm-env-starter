e392973977aab92a2dc8f10d0c34f73fff3b6cea
tw <>
2023-08-11 14:54:09 -0700

    Fixes .gitignore

---
+++ env/common_env.py
@@ -0,0 +1,5 @@
+# common_env.py
+common_settings = {
+    'REGION': 'en-us',
+    'SECRET_PASSWORD': '',
+}
---
+++ env/development_env.py
@@ -0,0 +1,11 @@
+# development_env.py
+from env.common_env import common_settings
+from env._development_secrets import (
+    secret_password,
+)
+
+settings = {
+    **common_settings,
+    'PORT': 8000,
+    'SECRET_PASSWORD': secret_password or '',
+}
